7001e023c19f9f5ce10e512f85935333073a8fbf
evgeny <evgeny@air.local>
2017-03-05 23:04:11 +0300

    simplified function "sing"

--- js/grid.js
+++ js/grid.js
@@ -282,9 +282,7 @@ HexagonGrid.prototype.getSelectedTile = function(mouseX, mouseY) {
 };
 
 
-HexagonGrid.prototype.sign = function(p1, p2, p3) {
-    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
-};
+
 
 HexagonGrid.prototype.clean = function() {
     if (this.block)
@@ -304,9 +302,11 @@ HexagonGrid.prototype.clean = function() {
 HexagonGrid.prototype.isPointInTriangle = function isPointInTriangle(pt, v1, v2, v3) {
     var b1, b2, b3;
 
-    b1 = this.sign(pt, v1, v2) < 0.0;
-    b2 = this.sign(pt, v2, v3) < 0.0;
-    b3 = this.sign(pt, v3, v1) < 0.0;
+    function sign(p1, p2, p3) { return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y); }
+    
+    b1 = sign(pt, v1, v2) < 0.0;
+    b2 = sign(pt, v2, v3) < 0.0;
+    b3 = sign(pt, v3, v1) < 0.0;
 
     return ((b1 == b2) && (b2 == b3));
 };
