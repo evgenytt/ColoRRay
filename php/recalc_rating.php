31e4a2ef75524e92b09d710da46bed8d73b008e3
Tatyana <elkesigoff@gmail.com>
2017-03-05 12:44:38 +0300

    NOW it does it (prev. commit)

--- php/recalc_rating.php
+++ php/recalc_rating.php
@@ -14,6 +14,12 @@
 	}
 	
 	$u = $_SESSION['username'];
+	$t = strval($_POST[timems]);
+	$cl = strval($_POST[level]);
+	
+	$sql = "UPDATE users SET level$cl=$t WHERE userid=".$_SESSION['username'].";";
+	$result = mysqli_query($con,$sql);
+	
 	mysqli_select_db($con,"colorray");
 	$sql = "SELECT levels_num FROM info";
 	$result = mysqli_query($con,$sql);
@@ -28,12 +34,10 @@
 	
 // 	$time = time() - strtotime(row['last_login']);
 	for($i = 0; $i < $l; $i++) {
-		$result = mysqli_query($con,$sql);
-		$top = mysqli_fetch_assoc($result);
 		$rating += 120000.0 / intval($row['level'.strval($i+1)]) + 1;
 	}
 	
-	$sql = "UPDATE users SET rating=".strval($rating)." WHERE userid=".$_SESSION['username'].";";
+	$sql = "UPDATE users SET rating=".strval($rating)." WHERE userid=$u;";
 	$result = mysqli_query($con,$sql);
 	
 	mysqli_close($con);
